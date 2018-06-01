4d3a6411fa7fc17e0921ab63a2e4686c3fa4afec
Guillermo Barila <gui365@yahoo.com.ar>
2018-05-31 22:10:52 -0400

    Created game as object and character as properties of that object

---
+++ assets/javascript/game.js
@@ -0,0 +1,11 @@
+var rpgGame = {
+    // Number of wins and losses
+    countWin: 0,
+    countLose: 0,
+    // Available chars from which to choose:
+    char1: { name: "Luke Skywalker", life: 150, attack: 8, counterAttack: 25, player: false, enemy: false },
+    char2: { name: "Jar Jar Bings", life: 120, attack: 5, counterAttack: 35, player: false, enemy: false },
+    char3: { name: "Yoda", life: 175, attack: 12, counterAttack: 15, player: false, enemy: false },
+    char4: { name: "Darth Vader", life: 250, attack: 10, counterAttack: 20, player: false, enemy: false },
+    
+};
