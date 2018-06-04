d5641d665aa2f11d30d584eac6fab3f413ae9fdc
Guillermo Barila <gui365@yahoo.com.ar>
2018-06-03 22:40:03 -0400

    Finished basic functionality. Need to work on graphics

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -49,7 +49,7 @@ var rpgGame = {
         $("#versus").remove();
         var button = $("<button>ATTACK</button>");
         button.attr("id", "attackButton");
-        button.on("click", rpgGame.attack);
+        button.click(rpgGame.attack);
         button.append("&nbsp;");
         button.append($("<i>").addClass("fas fa-arrow-circle-right"));
         $(".battle").append(button);
@@ -73,49 +73,82 @@ var rpgGame = {
     // This function will make the unselected characters available again after battle is finished
     charActive: function() {
         for (var i = 0; i < rpgGame.characters.length; i++) {
-            var charUnselected = $("#char" + i + "-gs");
+                var charUnselected = $("#char" + i + "-gs");
                 charUnselected.removeAttr("id");
                 charUnselected.attr("id", "char" + i);
                 charUnselected.addClass("hover-on");
-        }
+                charUnselected.removeClass("grey-font");    
+            }
     },
 
     attack: function() {
         // Get attack and life values from player, and the counter attack and life values from enemy
-        var playerIndex = $("#battle-player").find($(".char-card")).attr("data-id");
-        var playerAttack = rpgGame.characters[playerIndex].attack;
+        var player = $("#battle-player").find($(".char-card"));
+        var playerIndex = player.attr("data-id");
+        var playerAttack = parseInt(player.attr("data-attack"));
         // This will be the updated attack value after each press of the 'attack' button
-        var playerHitPoints = rpgGame.characters[playerIndex].attack;
-        var playerLife = rpgGame.characters[playerIndex].life;
+        var playerHitPoints = playerAttack;
+        var playerLife = parseInt(player.attr("data-life"));
         var playerName = rpgGame.characters[playerIndex].name;
         
-        var enemyIndex = $("#battle-enemy").find($(".char-card")).attr("data-id");
-        var enemyCounterAttack = rpgGame.characters[enemyIndex].counterAttack;
-        var enemyLife = rpgGame.characters[enemyIndex].life;
+        var enemy = $("#battle-enemy").find($(".char-card"));
+        var enemyIndex = enemy.attr("data-id");
+        var enemyCounterAttack = parseInt(enemy.attr("data-counter-attack"));
+        var enemyLife = parseInt(enemy.attr("data-life"));
         var enemyName = rpgGame.characters[enemyIndex].name;
-
+    
         // Substract player's attack value from enemy's life
-        enemyLife -= playerHitPoints;
-        playerHitPoints = playerHitPoints + playerAttack;
-        
-        
-        console.log(enemyLife);
-        console.log(rpgGame.characters[enemyIndex].life);
-        console.log(playerHitPoints);
-        
-        
-
-        $("#battle-result").text("You attacked " + enemyName + " for " + playerAttack + " damage");
-        
-
-        // Append the new life value to the enemy card
         // Increase player's attack value by its original value
-
+        playerHitPoints += rpgGame.characters[playerIndex].attack;
+        player.attr("data-attack", playerHitPoints);
+        // console.log("Hit points: " + playerHitPoints);
         
-        // Substract that value from player's life
+        enemyLife -= playerHitPoints;
+        enemy.attr("data-life", enemyLife);
+        // console.log("Enemy life: " + enemyLife);
+        
+        // Append the new life value to the enemy card
+        $("#battle-result").text("You attacked " + enemyName + " for " + playerAttack + " damage");    
+        $("#char-life"+enemyIndex).html("&nbsp;" + enemyLife);
 
-        // Conditional statement determines if player or enemy won
-        // Act accordingly
+        // Substract enemy's counter-attack value from player's life
+        playerLife -= enemyCounterAttack;
+        player.attr("data-life", playerLife);
+        $("#char-life"+playerIndex).html("&nbsp;" + playerLife);
+
+        // Conditional statements to determine if player won or lost
+        if (enemyLife <= 0) {
+            $("#battle-enemy").empty();
+            $("#battle-result").text("You have defeated " + enemyName);
+            $("#instructions").text("Pick your next opponent");
+            $(".battle").empty();
+            rpgGame.enemy = false;
+            rpgGame.charActive();
+            if ($("#char-select div").length === 0) {
+                $("#instructions").text("You win!");
+                $("#battle-result").text("You have defeated all enemies");
+                rpgGame.countWin++;
+                $("#count-win").text("Wins: " + rpgGame.countWin);
+                // console.clear();
+                // console.log(rpgGame.countWin);
+                setTimeout(restartGame, 4000);
+            }
+        }
+
+        if (playerLife <= 0) {
+            $("#battle-result").text("You have been defeated by " + enemyName);
+            $("#instructions").text("You lose!");
+            $(".battle").empty();
+            $("#battle-player").empty()
+            $("#battle-enemy").empty()
+            $("#char-select").empty()
+            rpgGame.enemy = false;
+            rpgGame.countLose++;
+            $("#count-lose").text("Losses: " + rpgGame.countLose);
+            // console.clear();
+            // console.log(rpgGame.countLose);
+            setTimeout(restartGame, 3000);
+        }
     }
 };
 
@@ -125,40 +158,62 @@ rpgGame.firstInstruction();
 
 
 // Dynamically creating the cards
-for (var i = 0; i < rpgGame.characters.length; i++) {
-    // Create a div for each character
-    var thisChar = "char" + i;
-    var charDiv = $("<div>");
-    charDiv.addClass("char-card hover-on char-img");
-    charDiv.attr("id", thisChar);
-    charDiv.attr("data-id", i);
-    charDiv.attr("data-life", rpgGame.characters[i].life);
-    $("#char-select").append(charDiv);
 
-    // Show its name and life on the card
-    var charLifeDiv = $("<div>");
-    charLifeDiv.addClass("char-stats-div");
-    charLifeDiv.attr("id", "char-stats" + i);
-    $("#char" + i).append(charLifeDiv);
+function createCards() {
+    for (var i = 0; i < rpgGame.characters.length; i++) {
+        // Create a div for each character
+        var thisChar = "char" + i;
+        var charDiv = $("<div>");
+        charDiv.addClass("char-card hover-on char-img");
+        charDiv.click(selectPlayerEnemy);
+        charDiv.attr("id", thisChar);
+        charDiv.attr("data-id", i);
+        charDiv.attr("data-life", rpgGame.characters[i].life);
+        charDiv.attr("data-attack", rpgGame.characters[i].attack);
+        charDiv.attr("data-counter-attack", rpgGame.characters[i].counterAttack);    
+        $("#char-select").append(charDiv);
 
-    var charName = $("<p>");
-    charName.html(rpgGame.characters[i].name);
-    charName.addClass("bold");
-    $("#char-stats" + i).append(charName);
+        // Show its name and life on the card
+        var charLifeDiv = $("<div>");
+        charLifeDiv.addClass("char-stats-div");
+        charLifeDiv.attr("id", "char-stats" + i);
+        $("#char" + i).append(charLifeDiv);
 
-    var charIcon = $("<i>");
-    charIcon.addClass("fa fa-heart");
-    $("#char-stats" + i).append(charIcon);
+        var charName = $("<p>");
+        charName.html(rpgGame.characters[i].name);
+        charName.addClass("bold");
+        $("#char-stats" + i).append(charName);
 
-    var charLife = $("<span>");
-    charLife.html("&nbsp;" + $("#char" + i).attr("data-life"));
-    $("#char-stats" + i).append(charLife);
+        var charIcon = $("<i>");
+        charIcon.addClass("fa fa-heart");
+        $("#char-stats" + i).append(charIcon);
+
+        var charLife = $("<span>");
+        charLife.attr("id", "char-life" + i)
+        charLife.html("&nbsp;" + $("#char" + i).attr("data-life"));
+        $("#char-stats" + i).append(charLife);
+    };
+};
+
+createCards();
+
+
+// Function to restart the game
+function restartGame() {
+    rpgGame.player = false;
+    rpgGame.enemy = false;
+    $("#battle-player").empty();
+    $("#battle-result").text("");
+    rpgGame.firstInstruction();
+    createCards();
 };
 
 
 // Choosing the player and enemy
-$(".char-card").on("click", function() {
+function selectPlayerEnemy() {
 
+    if ($(this).attr("data-selected") !== "true") {
+        
         if (rpgGame.player === false && rpgGame.enemy === false) {
             var selectedPlayer = "#char" + $(this).attr("data-id");
             $("#battle-player").append($(selectedPlayer));
@@ -184,32 +239,33 @@ $(".char-card").on("click", function() {
             $(selectedEnemy).attr("data-selected", true);
             $(selectedEnemy).removeClass("hover-on");
 
-            var repickButton = $("<button>Pick again</button>");
-            repickButton.click(rpgGame.repick);
-            $(".battle").append($(repickButton));
-            var changeIcon = $("<i>");
-            changeIcon.addClass("fas fa-sync");
-            repickButton.attr("id", "repickButton");
-            repickButton.prepend("&nbsp;");
-            repickButton.prepend(changeIcon);
+            if ($("#char-select div").length === 4) {
+                var repickButton = $("<button>Pick again</button>");
+                repickButton.click(rpgGame.repick);
+                $(".battle").append($(repickButton));
+                var changeIcon = $("<i>");
+                changeIcon.addClass("fas fa-sync");
+                repickButton.attr("id", "repickButton");
+                repickButton.prepend("&nbsp;");
+                repickButton.prepend(changeIcon);
 
-            var confirmButton = $("<button>Fight!</button>");
-            confirmButton.click(rpgGame.confirm);
-            confirmButton.attr("id", "confirmButton")
-            $(".battle").append($(confirmButton));
-            var changeIcon = $("<i>");
-            changeIcon.addClass("fas fa-thumbs-up");
-            confirmButton.prepend("&nbsp;");
-            confirmButton.prepend(changeIcon);
+                var confirmButton = $("<button>Fight!</button>");
+                confirmButton.click(rpgGame.confirm);
+                confirmButton.attr("id", "confirmButton")
+                $(".battle").append($(confirmButton));
+                var changeIcon = $("<i>");
+                changeIcon.addClass("fas fa-thumbs-up");
+                confirmButton.prepend("&nbsp;");
+                confirmButton.prepend(changeIcon);
+            } else {
+                rpgGame.confirm();
+            };
 
             $("#instructions").empty();
+            
         };
-
-        
-    });
-
-
-
+    }
+};
 
 
 // Animations
